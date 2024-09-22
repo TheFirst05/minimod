f76d216e515fd0c414b48b3a29c70309fe7358b3
TheFirst05 <96636889+TheFirst05@users.noreply.github.com>
2024-09-22 10:17:46 +0200

    Update first_minimod_scripts.js

--- scripts/first_minimod_scripts.js
+++ scripts/first_minimod_scripts.js
@@ -1,9 +1,12 @@
 console.log("minimod online");
 
-Hooks.on(SimpleCalendar.Hooks.DateTimeChange, (data) => {
-	//console.log(data);
-	if((data.date.hour == 5 && data.diff >= 3600) || (data.diff >= 86400)) { //runs if at least one hour advances forward into 5am, or if a full day is skipped
-		console.log("weather table triggered");
-		game.tables.getName("Weather Table").draw();
+Hooks.once("ready", (app, html, data) => { //ensures this runs last after Simple Calendar is fully loaded
+
+	Hooks.on(SimpleCalendar.Hooks.DateTimeChange, (data) => {
+		//console.log(data);
+		if (data.date.hour == 5 && data.diff >= 3600 || (data.diff >= 86400)) { //runs if at least one hour advances forward into 5am, or if a full day is skipped
+			console.log("weather table triggered");
+			game.tables.getName("Weather Table").draw();
 		}
- });
+	});
+});
