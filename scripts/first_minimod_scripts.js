c49e2ab7567a68e53d43f85bd96db67cd9ec1d29
TheFirst05 <96636889+TheFirst05@users.noreply.github.com>
2024-11-10 15:57:34 +0100

    stop multiple rolls

--- scripts/first_minimod_scripts.js
+++ scripts/first_minimod_scripts.js
@@ -22,12 +22,13 @@ Hooks.on("setup", () => {
   });
 
 Hooks.once("ready", (app, html, data) => { //ensures this runs last after Simple Calendar is fully loaded
-
 	Hooks.on(SimpleCalendar.Hooks.DateTimeChange, (data) => {
 		//console.log(data);
 		if (data.date.hour == 5 && data.diff >= 3600 || (data.diff >= 86400)) { //runs if at least one hour advances forward into 5am, or if a full day is skipped
-			console.log("weather table triggered");
-			game.tables.getName("Weather Table").draw();
+			if (game.user.isGM) { //otherwise it runs the script for each user logged in
+				console.log("weather table triggered");
+				game.tables.getName("Weather Table").draw();
+			}
 		}
 	});
 });
